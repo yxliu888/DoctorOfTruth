f8e79ff9e87cc17f4e221e3a941e1c0bfc862feb
yxliu888 <yxliu888@163.com>
2024-07-23 22:45:20 +0800

    build1

--- src/app/api/comments/route.ts
+++
@@ -1,9 +0,0 @@
-// 获取单篇文章的评论
-export async function getPostComments(postId: number) {
-    const response = await fetch(`http://localhost:8081/wordpress/wp-json/wp/v2/comments?post=${postId}`);
-    if (!response.ok) {
-      throw new Error('Failed to fetch comments');
-    }
-    const comments = await response.json();
-    return comments;
-  }
