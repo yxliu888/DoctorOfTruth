b979cd95da8b51753772291c1689f40021a0da5a
yxliu888 <yxliu888@163.com>
2024-07-24 11:11:56 +0800

    search

--- src/app/searchresults/page.tsx
+++ src/app/searchresults/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useEffect, useState, useRef } from 'react';
-import { useSearchParams, useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import api from '@/src/lib/api';
 import Image from 'next/image';
@@ -25,26 +25,15 @@ export interface Post {
 export default function SearchResult() {
   const router = useRouter();
   const [searchQuery, setSearchQuery] = useState('');
-  // const searchParams = useSearchParams();
-  useEffect(() => {
-    // 获取 URL 查询参数
-    const queryParams = new URLSearchParams(window.location.search);
-    const query = queryParams.get('query') || '';
-    // alert(query);
-    setSearchQuery(query);
-    const categoryId = queryParams.get('categoryId') || '';
-    //console.log('Search Term:', searchTerm); // 输出查询参数的值到控制台
-  }, []);
-  //const query = 'a'; //searchParams.get('query') || '';
-  const categoryId = ''; //searchParams.get('categoryId') || '';
-  
-
   const [posts, setPosts] = useState<Post[]>([]);
   const initialized = useRef(false);
   useEffect(() => {
     if (! initialized.current){
       initialized.current = true;
-      searchPosts(searchQuery, categoryId);
+      const queryParams = new URLSearchParams(window.location.search);
+      const query = queryParams.get('query') || '';
+      setSearchQuery(query);
+      searchPosts(query, '');
     }
   }, []);
   const searchPosts = async (searchQuery: string, categoryId: string) => {
@@ -56,6 +45,7 @@ export default function SearchResult() {
         postsRes = await api.get(`wp/v2/posts?search=${searchQuery}&categories=${categoryId}`);
       }
       const postsData = postsRes.data;
+      alert(searchQuery + postsRes.data.length);
       // Fetch authors data for each post
       const authorPromises = postsData.map(async (post: Post) => {
         const authorRes = await api.get(`wp/v2/users/${post.author}`);
@@ -76,39 +66,11 @@ export default function SearchResult() {
     router.push(`/searchresults`);
   }
   const categorySelect = async (categoryId: number, categoryName: string) => {
-    searchPosts(searchQuery, '' + categoryId);
+    searchPosts('', '' + categoryId);
   }
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
       <Navbar query={searchQuery} onQueryChange={handleQueryChange} handleSubmit={handleSubmit} />
-      {/* <header className="w-full flex justify-between items-center p-4 bg-white shadow-md">
-        <div className="flex items-center">
-          <Image src="/images/logo.png" alt="Logo" width={50} height={50} />
-          <h1 className="text-2xl font-bold ml-2">Doctor of Truth</h1>
-        </div>
-        <div className="flex-grow flex justify-center items-center">
-          <form onSubmit={handleSubmit}  className="flex items-center bg-gray-100 shadow-md rounded-full overflow-hidden px-2">
-            <input
-              type="text"
-              value={searchQuery}
-              placeholder="Search..."
-              onChange={(e) => setSearchQuery(e.target.value)}
-              className="flex-grow py-2 px-4 outline-none bg-transparent"
-            />
-            <button type="submit" className="text-gray-500 px-2">
-              <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-                <path fillRule="evenodd" d="M12.9 14.32a8 8 0 111.414-1.414l4.384 4.383a1 1 0 01-1.414 1.415l-4.384-4.383zM10 16a6 6 0 100-12 6 6 0 000 12z" clipRule="evenodd" />
-              </svg>
-            </button>
-          </form>
-        </div>
-        <div className="flex items-center space-x-4">
-          <a href="http://54.91.90.2/login" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Join the Community
-          </a>
-        </div>
-      </header> */}
-      {/* <main className="flex flex-col items-center mt-8"> */}
       <main className="flex flex-1 bg-white">
         <CategoryNav onCategorySelect={categorySelect}/>
         <div className="w-full max-w-4xl">
