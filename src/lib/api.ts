f593d1ef08b7cd99daa422ab69cdf7c4857ff79f
yxliu888 <yxliu888@163.com>
2024-07-27 13:46:55 +0800

    ip

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8081/wp-json/',
+    baseURL: 'http://54.242.114.85:8081/wp-json/',
     // baseURL: 'http://localhost:8081/wordpress/wp-json/',
   // 其他配置...
 });
